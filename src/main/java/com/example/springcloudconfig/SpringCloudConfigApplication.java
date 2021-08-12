0fbefb6f1c471bdd4c9c767a9654d518b435cb03
sunil <sunil.biswal63@gmail.com>
2021-08-12 16:20:17 +0530

    initial commit

--- src/main/java/com/example/springcloudconfig/SpringCloudConfigApplication.java
+++ src/main/java/com/example/springcloudconfig/SpringCloudConfigApplication.java
@@ -8,7 +8,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class SpringCloudConfigApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         SpringApplication.run(SpringCloudConfigApplication.class, args);
     }
 
